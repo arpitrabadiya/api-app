3d9234c0820a872fd0c5be5fab9a25ae799b64c2
arpitrabadiya <143240815+arpitrabadiya@users.noreply.github.com>
2023-08-26 11:23:07 +0530

    Fetch update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const bodyparser = require('body-parser')
-
+import express from 'express';
+import bodyparser from 'body-parser';
+import fetch from 'node-fetch';
 
 const app = express()
 app.use(express.json())
@@ -12,28 +12,27 @@ app.use(express.static('public'))
 
 
 app.get("/",async(req,res)=>{
-    
+    try{
         let limit = []
         res.render("task", {data: limit})
-    
-    
+    }
+    catch(err){
         console.log(err);
-    
+    }
     
 })
 
 app.post("/",async(req,res)=>{
-   
+    try{
         let token = req.body.token
 
     if(token===undefined) token="0x7213a321F1855CF1779f42c0CD85d3D95291D34C"
     console.log(token);
     let url ="https://api.dexscreener.com/latest/dex/tokens/"+token;
 
-    let response;
     
-        response = await fetch(url);
-
+    let response = await fetch(url);
+    
     const data= await response.json();
     const arrData = [data];
     let newdata = arrData[0].pairs
@@ -47,6 +46,10 @@ app.post("/",async(req,res)=>{
 
     limit.sort((a, b) => parseFloat(b.priceUsd) - parseFloat(a.priceUsd));
     res.render("task", {data: limit})
+    }
+    catch(err){
+        console.log(err);
+    }
 })
 
 let port = process.env.PORT || 3000
